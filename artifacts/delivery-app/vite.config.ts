1071bf6d439e4c820194102181c8fc963011e000
hamza515 <hmzhalmrwny1@gmail.com>
2026-04-29 23:13:21 +0300

    Update vite.config.ts

--- artifacts/delivery-app/vite.config.ts
+++ artifacts/delivery-app/vite.config.ts
@@ -62,6 +62,9 @@ export default defineConfig({
     port,
     host: "0.0.0.0",
     allowedHosts: true,
+    hmr: {
+      clientPort: 443, // <-- هذا هو الكود السحري الذي سيوقف التحديث المستمر
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
